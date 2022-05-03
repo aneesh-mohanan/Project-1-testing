0dd397a31e8d38e4c77f537ec521778510a4c5f8
aneesh-mohanan <aneeshcm182gmail.com>
2022-05-02 21:49:52 -0400

    deleted project1

--- Emailing/src/main/java/com/ex/services/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.ex.services;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
-import java.io.IOException;
-
-public interface EmailService {
-    void sendmail(String emailId, String message) throws AddressException, MessagingException, IOException;
-}
